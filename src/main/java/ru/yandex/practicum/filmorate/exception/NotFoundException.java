0ad208d52ce6eaa0846951ea609ee59914bd31d1
Anton Dragomir <anton.dragomir@yandex.ru>
2023-04-21 23:45:23 +0300

    codestyle rm unuse

--- src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
@@ -1,8 +1,5 @@
 package ru.yandex.practicum.filmorate.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
-
 public class NotFoundException extends AppException {
 
     public NotFoundException(String message) {
