ec60efa0fa5b31a4279e6bbb37cf19f70e7c831e
Anton Dragomir <anton.dragomir@yandex.ru>
2023-04-30 17:38:22 +0300

    { at the previous line

--- src/main/java/ru/yandex/practicum/filmorate/annotations/DescriptionValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/annotations/DescriptionValidator.java
@@ -4,8 +4,7 @@ package ru.yandex.practicum.filmorate.annotations;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-public class DescriptionValidator implements ConstraintValidator<DescriptionValidation, String>
-{
+public class DescriptionValidator implements ConstraintValidator<DescriptionValidation, String> {
     public boolean isValid(String description, ConstraintValidatorContext cxt) {
         return description == null || description.length() <= 200;
     }
--- src/main/java/ru/yandex/practicum/filmorate/annotations/LoginValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/annotations/LoginValidator.java
@@ -4,8 +4,7 @@ package ru.yandex.practicum.filmorate.annotations;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-public class LoginValidator implements ConstraintValidator<LoginValidation, String>
-{
+public class LoginValidator implements ConstraintValidator<LoginValidation, String> {
     public boolean isValid(String login, ConstraintValidatorContext cxt) {
         return (login != null) && !(login.isEmpty()) && !(login.contains(" "));
     }
