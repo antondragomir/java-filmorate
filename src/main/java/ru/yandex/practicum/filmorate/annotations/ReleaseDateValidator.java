96611e5e9b999e3ca8dfbeff173ed9f489f4f6b3
Anton Dragomir <anton.dragomir@yandex.ru>
2023-07-19 21:00:31 +0300

    notBeforeDate crazy changes

--- src/main/java/ru/yandex/practicum/filmorate/annotations/ReleaseDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/annotations/ReleaseDateValidator.java
@@ -5,9 +5,10 @@ import javax.validation.ConstraintValidatorContext;
 import java.time.LocalDate;
 
 public class ReleaseDateValidator implements ConstraintValidator<ReleaseDateValidation, LocalDate> {
-    private final LocalDate NOT_BEFORE_DATE = LocalDate.of(1895, 12, 28);
+    private final LocalDate notBeforeDate = LocalDate.of(1895, 12, 28);
+
     public boolean isValid(LocalDate releaseDate, ConstraintValidatorContext cxt) {
         if (releaseDate == null) return false;
-        return releaseDate.isAfter(NOT_BEFORE_DATE);
+        return releaseDate.isAfter(notBeforeDate);
     }
 }
